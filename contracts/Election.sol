8764f5ec82d276005d07ada582e0097eb80612ed
Sahil Singh <sahilsingh.dsc@gmail.com>
2019-03-20 21:08:31 +0530

    2. addCandidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,15 +1,31 @@
 pragma solidity >=0.4.22;
 
 contract Election {
-    // Constructor
-    // Store Candidate
-    // Read Candidate
+    //Model a Candidate
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store Candidates
+    // Fetch Candidates
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+
 
     string public candidate;
 
     constructor () public {
-        candidate = "Candidate 1";
-
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
     }
 
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
 }
