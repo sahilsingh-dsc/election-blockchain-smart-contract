9cd63d0eb2b4c0e6c0ad8d4bc1ddaae1f48bdf8f
Sahil Sinh <sahilsingh.dsc@gmail.com>
2019-03-20 18:56:04 +0530

    1. Smoke Test

---
+++ contracts/Election.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.4.22;
+
+contract Election {
+    // Constructor
+    // Store Candidate
+    // Read Candidate
+
+    string public candidate;
+
+    constructor () public {
+        candidate = "Candidate 1";
+
+    }
+
+}
